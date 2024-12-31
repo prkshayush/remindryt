b8961d71372c04f851f8ba05e099bd7b3010d3b1
prkshayush <itsnotmymailid140@gmail.com>
2024-12-31 23:24:53 +0530

    ui changes

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Oregano } from "next/font/google";
 import "./globals.css";
 import { ReduxProvider } from "@/lib/store/provider";
 
@@ -10,6 +10,12 @@ const geistSans = Geist({
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const oreganoSans = Oregano({
+  variable: "--font-oregano-sans",
+  weight: '400',
   subsets: ["latin"],
 });
 
@@ -26,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${oreganoSans.variable} antialiased`}
       >
         <ReduxProvider>
           {children}
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,9 +1,10 @@
 
+import AboutSection from '@/components/AboutSection';
 import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center gap-10 font-[family-name:var(--font-geist-mono)]">
+    <main className="flex flex-col items-center gap-10 font-[family-name:var(--font-geist-sans)]">
       <nav className="w-full">
         <ul className="container mx-auto flex justify-between items-center py-3">
           <li className="list-type-none py-1 px-4 rounded-xl border-b-2 border-r-2 border-blue-600 hover:border-green-700">
@@ -15,16 +16,19 @@ export default function Home() {
         </ul>
       </nav>
       <div className="my-20 flex flex-col items-center justify-center">
-        <h1 className="sm:text-6xl font-sans sm:font-bold px-4 py-2 text-3xl font-semibold">Smash Deadlines Leverage AI, Win Every Task.</h1>
+        <h1 className="sm:text-7xl sm:font-bold px-4 py-2 text-3xl font-semibold font-[family-name:var(--font-oregano-sans)]">Smash Deadlines Leverage AI, Win Every Task.</h1>
         <p className="sm:text-3xl py-1 px-4 text-center">Stay in sync, compete smart, and achieve greatness together.</p>
         <div>
           <button className="py-1 px-4 rounded-xl border-t-2 border-l-2 border-blue-600 hover:border-green-700"><a href="/dashboard">Get Started</a></button>
-          <button className="py-1 px-4 rounded-xl border-b-2 border-r-2 border-green-700 hover:border-blue-600"><a href="#learn">Learn More</a></button>
+          <button className="py-1 px-4 rounded-xl border-b-2 border-r-2 border-green-700 hover:border-blue-600"><a href="#about">Learn More</a></button>
         </div>
       </div>
       <div className="my-28">
         <p className="sm:text-3xl sm:font-semibold py-1 px-4 text-center">Enjoy AI-powered scoring, real-time progress tracking, and personalized motivation.</p>
-        <p className="sm:text-3xl py-1 px-4 text-center">With RemindRyt, turn task management into a team sport.</p>
+        <p className="sm:text-3xl py-1 px-4 text-center">With <span className='sm:text-4xl font-semibold font-[family-name:var(--font-oregano-sans)]'>RemindRyt</span>, turn task management into a team sport.</p>
+      </div>
+      <div className='w-full' id='about'>
+        <AboutSection />
       </div>
     </main>
   )
